50d7fb61686cab9b8a9249ffb8601fa54fd657a2
victorlabos <labosvictor1@gmail.com>
2023-03-10 21:44:49 -0300

    inicial

---
+++ app/Models/farmacia.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class farmacia extends Model
+{
+    protected $table = "gatos";
+
+    // Indica os campos que podem ser preenchidos pelo usuário
+    protected $fillable = [
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
+    ];
+}
